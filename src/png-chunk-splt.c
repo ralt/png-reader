ca99325400ec5ac15e6536bf3315def557d8f1de
Florian Margaine <florian@margaine.com>
2013-12-30 00:23:13 +0100

    Adding sPLT structs

---
+++ src/png-chunk-splt.c
@@ -0,0 +1,69 @@
+#include "png-chunk-splt.h"
+
+bool PNG_chunk_sPLT_check_type(struct PNG_chunk *chunk)
+{
+	uint8_t const defaults[] = { 0x73, 0x50, 0x4c, 0x54 };
+	for (size_t i = 0; i < PNG_header_type_size; i++) {
+		if (defaults[i] != chunk->type[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void PNG_chunk_sPLT_vector_init(struct PNG_chunk_sPLT_vector *vector,
+				int capacity)
+{
+	vector->size = 0;
+	vector->capacity = capacity;
+	vector->chunks = malloc(sizeof(struct PNG_chunk) * vector->capacity);
+	if (vector->chunks == NULL) {
+		printf("%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	if (vector->chunks == NULL) {
+		exit(errno);
+	}
+}
+
+void
+PNG_chunk_sPLT_vector_append(struct PNG_chunk_sPLT_vector *vector,
+			     struct PNG_chunk *chunk)
+{
+	if (vector->size >= vector->capacity) {
+		vector->capacity *= 2;
+		vector->chunks = realloc(vector->chunks,
+					 sizeof(struct PNG_chunk) *
+					 vector->capacity);
+		if (vector->chunks == NULL) {
+			exit(errno);
+		}
+	}
+	vector->chunks[vector->size++] = chunk;
+}
+
+struct PNG_chunk_sPLT *PNG_chunk_sPLT_vector_get(struct PNG_chunk_sPLT_vector
+						 *vector, int index)
+{
+	if (index < 0 || index >= vector->size) {
+		printf("Out of bounds\n");
+		exit(1);
+	}
+	return vector->chunks[index];
+}
+
+void
+PNG_chunk_sPLT_vector_set(struct PNG_chunk_sPLT_vector *vector, int index,
+			  struct PNG_chunk_sPLT *value)
+{
+	vector->chunks[index] = value;
+}
+
+void PNG_chunk_sPLT_vector_free(struct PNG_chunk_sPLT_vector *vector)
+{
+	for (size_t i = 0; i < vector->size; i++) {
+		PNG_chunk_sPLT_free(PNG_chunk_sPLT_vector_get(vector, i));
+	}
+	free(vector);
+}
---
+++ src/png-chunk-splt.h
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifndef PNG_CHUNK_SPLT
+#define PNG_CHUNK_SPLT
+
+#include "png-chunk.h"
+
+struct PNG_chunk_sPLT_palette_entry {
+	unsigned int red;
+	unsigned int green;
+	unsigned int blue;
+	unsigned int alpha;
+	unsigned int frequency;
+};
+
+struct PNG_chunk_sPLT {
+	size_t index;
+
+	char *palette_name;
+	uint8_t sample_depth;
+
+	PNG_chunk_sPLT_palette_entry **palette_entries;
+};
+
+struct PNG_chunk_sPLT_vector {
+	size_t size;
+	int capacity;
+	struct PNG_chunk_sPLT **chunks;
+};
+
+/**
+ * Checks that the chunk is an sPLT chunk.
+ */
+bool PNG_chunk_sPLT_check_type(struct PNG_chunk *chunk);
+
+/**
+ * Initializes the memory for a vector of chunks with defined capacity.
+ */
+void PNG_chunk_sPLT_vector_init(struct PNG_chunk_sPLT_vector *vector,
+				int capacity);
+
+/**
+ * Appends a PNG_chunk on the vector. If needed, it reallocates memory.
+ */
+void PNG_chunk_sPLT_vector_append(struct PNG_chunk_sPLT_vector *vector,
+				  struct PNG_chunk_sPLT *chunk);
+
+/**
+ * Returns the PNG_chunk at index.
+ */
+struct PNG_chunk_sPLT *PNG_chunk_sPLT_vector_get(struct PNG_chunk_sPLT_vector
+						 *vector, int index);
+
+/**
+ * Defines the PNG_chunk stored at index.
+ */
+void PNG_chunk_sPLT_vector_set(struct PNG_chunk_sPLT_vector *vector, int index,
+			       struct PNG_chunk_sPLT *value);
+
+/**
+ * Frees the memory for the vector.
+ */
+void PNG_chunk_sPLT_vector_free(struct PNG_chunk_sPLT_vector *vector);
+
+/**
+ * Frees the memory of a single sPLT chunk.
+ */
+void PNG_chunk_sPLT_free(struct PNG_chunk_sPLT *chunk)
+{
+	free(chunk->palette_name);
+	free(chunk->palette_entries);
+	free(chunk);
+}
+
+#endif
